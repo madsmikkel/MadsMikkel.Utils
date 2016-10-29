f42eadbfcc3e142634ddeb26ef3f3c0ea3788be8
madsmikkel <mads.mikkel@outlook.com>
2016-10-29 10:33:43 +0200

    Added Object overrides and interface member declarations.

--- MadsMikkel.Utils.Numerics/BigDecimal.cs
+++ MadsMikkel.Utils.Numerics/BigDecimal.cs
@@ -4,7 +4,41 @@ using System.Numerics;
 
 namespace MadsMikkel.Utils.Numerics
 {
-    public class BigDecimal
-    {
-    }
+	public struct BigDecimal : IFormattable, IComparable, IComparable<BigDecimal>, IEquatable<BigDecimal>
+	{
+		public override bool Equals( object obj )
+		{
+			return base.Equals( obj );
+		}
+
+		public override int GetHashCode()
+		{
+			return base.GetHashCode();
+		}
+
+		public override string ToString()
+		{
+			return base.ToString();
+		}
+
+		int IComparable<BigDecimal>.CompareTo( BigDecimal other )
+		{
+			throw new NotImplementedException();
+		}
+
+		int IComparable.CompareTo( object obj )
+		{
+			throw new NotImplementedException();
+		}
+
+		bool IEquatable<BigDecimal>.Equals( BigDecimal other )
+		{
+			throw new NotImplementedException();
+		}
+
+		string IFormattable.ToString( string format, IFormatProvider formatProvider )
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
