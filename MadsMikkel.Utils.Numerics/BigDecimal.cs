e9cdf543b784c78d4a478ab289410e7794e2ecf2
madsmikkel <mads.mikkel@outlook.com>
2016-10-29 10:29:05 +0200

    Added ref and using to System.Numerics. Added CLS compliance asembly attribute.

--- MadsMikkel.Utils.Numerics/BigDecimal.cs
+++ MadsMikkel.Utils.Numerics/BigDecimal.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Numerics;
+[assembly:CLSCompliant(true)]
 
 namespace MadsMikkel.Utils.Numerics
 {
