7ae8a5022cb1b2d644fc41743e8c3703923b1ba4
Mads Mikkel Rasmussen <mads.mikkel@outlook.com>
2016-12-19 08:54:56 +0100

    Added List<TEven, TOdd>.

---
+++ MadsMikkel.Utils.DataStructures/Class1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadsMikkel.Utils.DataStructures
+{
+
+	/// <summary>
+	/// A generic linear data structure, that accepts two types as type parameters.
+	/// </summary>
+	/// <typeparam name="TEven">The type that will be contained at even indices.</typeparam>
+	/// <typeparam name="TOdd">The type that will be contained at odd indices.</typeparam>
+	public class List<TEven, TOdd>
+	{
+		public object this[int index]
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+
+			set
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public int Count
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool IsFixedSize
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool IsReadOnly
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool IsSynchronized
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public object SyncRoot
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public int Add(TEven value)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int Add(TOdd value)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Clear()
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Contains(object value)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void CopyTo(Array array, int index)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerator GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+
+		public int IndexOf(object value)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Insert(int index, object value)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Remove(object value)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void RemoveAt(int index)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
